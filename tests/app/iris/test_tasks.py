02b4474223a2a388908b4761bf9ae7140601b4b5
jpfcabral <jpfcabral@gmail.com>
2024-05-13 23:49:32 -0300

    add remaining tests

--- tests/app/iris/test_tasks.py
+++ tests/app/iris/test_tasks.py
@@ -3,6 +3,7 @@ from unittest.mock import patch
 
 from app.iris.tasks import predict
 from app.iris.tasks import predict_batch
+from app.iris.tasks import train_iris_model
 
 
 @patch("app.iris.tasks.IrisPredictionService")
@@ -20,3 +21,15 @@ def test_predict_batch(IrisPredictionService):
     predict_batch(model_id, data)
     IrisPredictionService.assert_called_once_with(model_id)
     IrisPredictionService.return_value.predict_batch.assert_called_once_with(data)
+
+
+@patch("app.iris.tasks.mlflow")
+@patch("app.iris.tasks.pd")
+@patch("app.iris.tasks.train_test_split")
+@patch("app.iris.tasks.keras")
+def test_train_iris_model(keras, train_test_split, pd, mlflow):
+    train_test_split.return_value = [MagicMock(), MagicMock(), MagicMock(), MagicMock()]
+    model = MagicMock()
+    model.evaluate.return_value = (0.2, 0.3)
+    keras.models.Sequential.return_value = model
+    train_iris_model("data")
